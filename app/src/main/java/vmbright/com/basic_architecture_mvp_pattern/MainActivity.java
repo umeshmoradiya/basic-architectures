5a7f6bedab2f9897aab9b9a86f3a3f190160500d
umeshmoradiya <umeshmoradiya@venb4311.ad.huf>
2017-01-03 09:02:53 +0100

    initial commit

---
+++ app/src/main/java/vmbright/com/basic_architecture_mvp_pattern/MainActivity.java
@@ -0,0 +1,13 @@
+package vmbright.com.basic_architecture_mvp_pattern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
